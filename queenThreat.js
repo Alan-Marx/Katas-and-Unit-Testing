a6bb57fd3a0945076e687348160301ab1fc34c8c
Alan-Marx <alanmarx@hotmail.com>
2020-06-26 17:49:26 -0700

    first commit

---
+++ queenThreat.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// The following function detects whether two queens on a chessboard can attack each other.
+// it is assumed that there are only two queens on the board. 
+
+const queenThreat = (queen1, queen2) => {
+  
+  if (queen1[0] === queen2[0] || queen1[1] === queen2[1]) {
+    return true;
+  } 
+  const topQueen = queen1[0] < queen2[0] ? queen1 : queen2;
+  const bottomQueen = queen1[0] < queen2[0] ? queen2 : queen1;
+  const rowDistance = bottomQueen[0] - topQueen[0];
+  
+  if (bottomQueen[1] === topQueen[1] - rowDistance || bottomQueen[1] === topQueen[1] + rowDistance) {
+    return true;
+  }
+  return false;
+}
+
+console.log(queenThreat([0, 5], [5, 0]));
+console.log(queenThreat([0, 0], [5, 7]));
+console.log(queenThreat([0, 0], [0, 7]));
+console.log(queenThreat([4, 3], [1, 3]));
+console.log(queenThreat([1, 0], [4, 3]));
