f8cda59f77cb5a6f65ae18f656f5a40a938f79a4
Alan-Marx <alanmarx@hotmail.com>
2020-06-29 11:25:37 -0700

    added expect chai assertions to the test statements for the queenThreat function

--- queenThreat.js
+++ queenThreat.js
@@ -20,8 +20,3 @@ const queenThreat = (queen1, queen2) => {
 
 module.exports = { queenThreat };
 
-// console.log(queenThreat([0, 5], [5, 0]));
-// console.log(queenThreat([0, 0], [5, 7]));
-// console.log(queenThreat([0, 0], [0, 7]));
-// console.log(queenThreat([4, 3], [1, 3]));
-// console.log(queenThreat([1, 0], [4, 3]));
--- test/test.js
+++ test/test.js
@@ -1,22 +1,20 @@
 const expect = require('chai').expect;
 const { queenThreat } = require('../queenThreat');
 
-console.log(queenThreat);
-
 describe('Queen Threat Function', () => {
   it('should return true if two queens on a chessboard are in a position to attack each other vertically', () => {
-    
+    expect(queenThreat([0, 3], [3, 3])).to.be.true;
   });
 
   it('should return true if two queens on a chessboard are in a position to attack each other horizontally', () => {
-    
-  });
+    expect(queenThreat([2, 1], [2, 5])).to.be.true;
+  }); 
 
   it('should return true if two queens on a chessboard are in a position to attack each other diagonally', () => {
-    
+    expect(queenThreat([0, 5], [5, 0])).to.be.true;
   });
 
   it('should return false if two queens on a chessboard are not in a position to attack each other', () => {
-    
+    expect(queenThreat([4, 1], [5, 4])).to.be.false;
   });
 });
